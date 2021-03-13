0ad7cf23e32bf3b3beacc8676e72c3056b1536a4
Positron832 <72881516+Positron832@users.noreply.github.com>
2021-03-13 17:57:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ let startBtn = document.getElementById("startBtn");
 
 startBtn.onmouseover = function(){infoAppear()};
 startBtn.onmouseout = function(){infoDissappear()};
+let infoTxt = document.createElement("p");
 
 function infoAppear() {
-  let infoTxt = document.createElement("p");
   infoTxt.textContent= "Start the game?";
   document.body.appendChild(infoTxt);
 }
 
 function infoDissappear() {
-  startBtn.remove();
+  infoTxt.remove();
 }
