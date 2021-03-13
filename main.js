205a055950dad69c07bf67448cce737c4c984fde
Positron832 <72881516+Positron832@users.noreply.github.com>
2021-03-13 23:27:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 let startBtn = document.getElementById("startBtn");
+let credits = document.getElementById("credits");
 
 startBtn.onmouseover = function(){infoAppear()};
 startBtn.onmouseout = function(){infoDissappear()};
+credits.onmouseover = function(){infoAppear2()};
+credits.onmouseout = function(){infoDissappear()};
 let infoTxt = document.createElement("p");
 
 function infoAppear() {
   infoTxt.textContent= "Start the game?";
   document.body.appendChild(infoTxt);
 }
+
+function infoAppear2() {
+  infoTxt.textContent = "See credits";
+  document.body.appendChild(infoTxt);
 
 function infoDissappear() {
   infoTxt.remove();
