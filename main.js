f3c1a240189670f47288b28ba26bc62d7707dfc6
Positron832 <72881516+Positron832@users.noreply.github.com>
2021-03-13 13:10:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let startBtn = document.getElementById("startBtn");
 startBtn.onmouseover {
   let infoTxt = document.createElement("p");
   infoTxt.innerHTML = "Start the game?";
+  document.body.appendChild(infoTxt);
 }
 
 
