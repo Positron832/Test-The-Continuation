2ad295adc86e6b2884c81acf2e10ea7d19374871
Positron832 <72881516+Positron832@users.noreply.github.com>
2021-03-15 10:40:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let credits = document.getElementById("credits");
 let title = document.getElementById("menuTxt");
 let titleMusic = document.getElementById("titleMusic");
 
-titleMusic.volume = 0.5;
+documemt.querySelectorAll("audio").volume = 0.3;
 
 document.body.onload = function(){bodyOnload()};
 function bodyOnload() {
